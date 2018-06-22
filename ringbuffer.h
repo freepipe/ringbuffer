52fccc0329ff99ac02ba0b17f5e1906d75b5ba04
chong <chong@chong.debian>
2018-06-22 19:59:06 +0800

    update

--- ringbuffer.h
+++ ringbuffer.h
@@ -4,18 +4,18 @@
 #define MIN(x, y)	((x) > (y) ? (y) : (x))
 #define MAX(x, y)	((x) > (y) ? (x) : (y))
 
-#define RingBuffer_Length(x)		(x.len)
-#define RingBuffer_Size(x)  		(sizeof(x.data))
-#define RingBuffer_Free(x)			(sizeof(x.data) - x.len)
-#define RingBuffer_Data(x,i)		(x.data[x.beg + (i)])
+#define RingBuffer_Length(x)        (x.len)
+#define RingBuffer_Size(x)          (sizeof(x.data))
+#define RingBuffer_Free(x)          (sizeof(x.data) - x.len)
+#define RingBuffer_Data(x,i)        (x.data[x.beg + (i)])
 
-#define RingBuffer_Init(x)			(x.len = x.beg = x.end = 0)
-#define RingBuffer_PopBack(x)		(RingBuffer_Length(x) > 0 ? (x.end--, x.len--) : 0)
-#define RingBuffer_PopFront(x)		(RingBuffer_Length(x) > 0 ? (x.beg++, x.len--) : 0)
-#define RingBuffer_PushBack(x, c)	(RingBuffer_Free(x)   > 0 ? ((x.data[x.end++] = (char)c), x.len++) : 0)
-#define RingBuffer_PushFront(x, c)	(RingBuffer_Free(x)   > 0 ? ((x.data[--x.beg] = (char)c), x.len++) : 0)
-#define RingBuffer_PopBackL(x, l)	do{int len = MIN(x.len, l); x.end -= len; x.len -= len;}while(0)
-#define RingBuffer_PopFrontL(x, l)	do{int len = MIN(x.len, l); x.beg += len; x.len -= len;}while(0)
+#define RingBuffer_Init(x)          (x.len = x.beg = x.end = 0)
+#define RingBuffer_PopBack(x)       (RingBuffer_Length(x) > 0 ? (x.end--, x.len--) : 0)
+#define RingBuffer_PopFront(x)      (RingBuffer_Length(x) > 0 ? (x.beg++, x.len--) : 0)
+#define RingBuffer_PushBack(x, c)   (RingBuffer_Free(x)   > 0 ? ((x.data[x.end++] = (char)c), x.len++) : 0)
+#define RingBuffer_PushFront(x, c)  (RingBuffer_Free(x)   > 0 ? ((x.data[--x.beg] = (char)c), x.len++) : 0)
+#define RingBuffer_PopBackL(x, l)   do{int len = MIN(x.len, l); x.end -= len; x.len -= len;}while(0)
+#define RingBuffer_PopFrontL(x, l)  do{int len = MIN(x.len, l); x.beg += len; x.len -= len;}while(0)
 
 #define RingBuffer_PeekData(x, d, l) \
 do{\
