3c20358326aa4f5d12ec0cf6aedfec545f50164d
chong <chong@chong.debian>
2018-07-09 17:04:37 +0800

    update

--- ringbuffer.c
+++ ringbuffer.c
@@ -3,23 +3,23 @@
 
 int main(int argc, char **argv)
 {
-	//RingBuffer256 buffer;
-	//RingBuffer512 buffer;
-	RingBuffer1024 buffer;
+	//ringbuffer256 buffer;
+	//ringbuffer512 buffer;
+	ringbuffer1024 buffer;
 
-	RingBuffer_Init(buffer);
-	for(int i = 0; i < RingBuffer_Size(buffer) * 2; i++)
+	ringbuffer_init(buffer);
+	for(int i = 0; i < ringbuffer_size(buffer) * 2; i++)
 	{
-		RingBuffer_PushBack(buffer, i);
+		ringbuffer_pushback(buffer, i);
 	}
 	printf("buffer:{len=%d, beg=%d, end=%d}\n", buffer.len, buffer.beg, buffer.end);
-	for(int i = 0; i < RingBuffer_Size(buffer) * 2; i++)
+	for(int i = 0; i < ringbuffer_size(buffer) * 2; i++)
 	{
 		if( i % 90 == 89 )
 		{
 			printf("buffer:{len=%d, beg=%d, end=%d}\n", buffer.len, buffer.beg, buffer.end);
-			RingBuffer_PopBackL(buffer, 1);
-			RingBuffer_PopFrontL(buffer, 1);
+			ringbuffer_popbackl(buffer, 1);
+			ringbuffer_popfrontl(buffer, 1);
 		}
 	}
 	printf("buffer:{len=%d, beg=%d, end=%d}\n", buffer.len, buffer.beg, buffer.end);
--- ringbuffer.h
+++ ringbuffer.h
@@ -1,42 +1,42 @@
 #ifndef __RING_BUFFER_H__
 #define __RING_BUFFER_H__
 
-#define MIN(x, y)	((x) > (y) ? (y) : (x))
-#define MAX(x, y)	((x) > (y) ? (x) : (y))
+#define min(x, y)	((x) > (y) ? (y) : (x))
+#define max(x, y)	((x) > (y) ? (x) : (y))
 
-#define RingBuffer_Length(x)        (x.len)
-#define RingBuffer_Size(x)          (sizeof(x.data))
-#define RingBuffer_Free(x)          (sizeof(x.data) - x.len)
-#define RingBuffer_Data(x,i)        (x.data[(x.beg + (i)) % sizeof(x.data)])
+#define ringbuffer_length(x)        ((x).len)
+#define ringbuffer_size(x)          (sizeof((x).data))
+#define ringbuffer_free(x)          (sizeof((x).data) - (x).len)
+#define ringbuffer_data(x,i)        ((x).data[((x).beg + (i)) % sizeof((x).data)])
 
-#define RingBuffer_Init(x)          (x.len = x.beg = x.end = 0)
-#define RingBuffer_PopBack(x)       (RingBuffer_Length(x) > 0 ? (x.end--, x.len--) : 0)
-#define RingBuffer_PopFront(x)      (RingBuffer_Length(x) > 0 ? (x.beg++, x.len--) : 0)
-#define RingBuffer_PushBack(x, c)   (RingBuffer_Free(x)   > 0 ? ((x.data[x.end++] = (char)c), x.len++) : 0)
-#define RingBuffer_PushFront(x, c)  (RingBuffer_Free(x)   > 0 ? ((x.data[--x.beg] = (char)c), x.len++) : 0)
-#define RingBuffer_PopBackL(x, l)   do{int len = MIN(x.len, l); x.end -= len; x.len -= len;}while(0)
-#define RingBuffer_PopFrontL(x, l)  do{int len = MIN(x.len, l); x.beg += len; x.len -= len;}while(0)
+#define ringbuffer_init(x)          ((x).len = (x).beg = (x).end = 0)
+#define ringbuffer_popback(x)       (ringbuffer_length(x) > 0 ? ((x).end--, (x).len--) : 0)
+#define ringbuffer_popfront(x)      (ringbuffer_length(x) > 0 ? ((x).beg++, (x).len--) : 0)
+#define ringbuffer_pushback(x, c)   (ringbuffer_free(x)   > 0 ? (((x).data[(x).end++] = (char)(c)), (x).len++) : 0)
+#define ringbuffer_pushfront(x, c)  (ringbuffer_free(x)   > 0 ? (((x).data[--(x).beg] = (char)(c)), (x).len++) : 0)
+#define ringbuffer_popbackl(x, l)   do{int len = min((x).len, l); (x).end -= len; (x).len -= len;}while(0)
+#define ringbuffer_popfrontl(x, l)  do{int len = min((x).len, l); (x).beg += len; (x).len -= len;}while(0)
 
-#define RingBuffer_PeekData(x, d, l) \
+#define ringbuffer_peekdata(x, d, l) \
 do{\
 	int i = 0;\
-	int len = MIN(x.len, l);\
-	unsigned char beg = x.beg;\
+	int len = min((x).len, l);\
+	unsigned char beg = (x).beg;\
 	for(i = 0; i < len; i++)\
 	{\
-		d[i] = x.data[beg++];\
+		(d)[i] = (x).data[beg++];\
 	}\
 }while(0)
 
-#define RingBuffer_GetData(x, d, l) \
+#define ringbuffer_getdata(x, d, l) \
 do{\
 	int i = 0;\
-	int len = MIN(x.len, l);\
+	int len = min((x).len, (l));\
 	for(i = 0; i < len; i++)\
 	{\
-		d[i] = x.data[x.beg++];\
+		(d)[i] = (x).data[(x).beg++];\
 	}\
-	x.len -= len;\
+	(x).len -= len;\
 }while(0)
 
 typedef struct
@@ -46,7 +46,7 @@ typedef struct
 	unsigned end:8;
 	unsigned mtx:1;
 	char data[256];
-}RingBuffer256;
+}ringbuffer256;
 
 typedef struct
 {
@@ -55,7 +55,7 @@ typedef struct
 	unsigned end:9;
 	unsigned mtx:1;
 	char data[512];
-}RingBuffer512;
+}ringbuffer512;
 
 typedef struct
 {
@@ -64,9 +64,9 @@ typedef struct
 	unsigned end:10;
 	unsigned mtx:1;
 	char data[1024];
-}RingBuffer1024;
+}ringbuffer1024;
 
-typedef RingBuffer256 RingBuffer;
+typedef ringbuffer256 ringbuffer;
 
 
 #endif//__RING_BUFFER_H__
